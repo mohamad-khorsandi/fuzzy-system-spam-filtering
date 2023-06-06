d29e95efe756dc51ca672b6d7e6d46b038c3c15b
mohammad <9m.kh557@gmai.com>
2023-06-06 09:51:15 +0330

    name change

--- fuzzy_system/fuzzy_set.py
+++ fuzzy_system/fuzzy_set.py
@@ -14,18 +14,18 @@ class FuzzySet:
 		self._res = res
 
 		self._domain = np.linspace(domain_min, domain_max, res)
-		self._dom = np.zeros(self._domain.shape)
+		self._membership_degree_list = np.zeros(self._domain.shape)
 		self._name = name
 		self._last_dom_value = 0
 
 	def __getitem__(self, x_val):
-		return self._dom[np.abs(self._domain-x_val).argmin()]
+		return self._membership_degree_list[np.abs(self._domain - x_val).argmin()]
 
 	def __setitem__(self, x_val, dom):
-		self._dom[np.abs(self._domain-x_val).argmin()] = round(dom, self._precision)
+		self._membership_degree_list[np.abs(self._domain - x_val).argmin()] = round(dom, self._precision)
 
 	def __str__(self):
-		return ' + '.join([str(a) + '/' + str(b) for a,b in zip(self._dom, self._domain)])
+		return ' + '.join([str(a) + '/' + str(b) for a,b in zip(self._membership_degree_list, self._domain)])
 
 	def __get_last_dom_value(self):
 		return self._last_dom_value
@@ -41,7 +41,7 @@ class FuzzySet:
 
 	@property
 	def empty(self):
-		return np.all(self._dom == 0)
+		return np.all(self._membership_degree_list == 0)
 
 	@property
 	def name(self):
@@ -56,7 +56,7 @@ class FuzzySet:
 		c = t1fs._adjust_domain_val(c)
 		d = t1fs._adjust_domain_val(d)
 
-		t1fs._dom = np.round(np.minimum(np.maximum(np.minimum((t1fs._domain-a)/(b-a), (d-t1fs._domain)/(d-c)), 0), 1), t1fs._precision)
+		t1fs._membership_degree_list = np.round(np.minimum(np.maximum(np.minimum((t1fs._domain - a) / (b - a), (d - t1fs._domain) / (d - c)), 0), 1), t1fs._precision)
 		return t1fs
 
 	@classmethod
@@ -68,11 +68,11 @@ class FuzzySet:
 		c = t1fs._adjust_domain_val(c)
 
 		if b == a:
-			t1fs._dom = np.round(np.maximum((c-t1fs._domain)/(c-b), 0), t1fs._precision)
+			t1fs._membership_degree_list = np.round(np.maximum((c - t1fs._domain) / (c - b), 0), t1fs._precision)
 		elif b == c:
-			t1fs._dom = np.round(np.maximum((t1fs._domain-a)/(b-a), 0), t1fs._precision)
+			t1fs._membership_degree_list = np.round(np.maximum((t1fs._domain - a) / (b - a), 0), t1fs._precision)
 		else:
-			t1fs._dom = np.round(np.maximum(np.minimum((t1fs._domain-a)/(b-a), (c-t1fs._domain)/(c-b)), 0), t1fs._precision)
+			t1fs._membership_degree_list = np.round(np.maximum(np.minimum((t1fs._domain - a) / (b - a), (c - t1fs._domain) / (c - b)), 0), t1fs._precision)
 		
 		return t1fs
 
@@ -80,51 +80,51 @@ class FuzzySet:
 		return self._domain[np.abs(self._domain-x_val).argmin()]
 
 	def clear_set(self):
-		self._dom.fill(0)
+		self._membership_degree_list.fill(0)
 
 	def min_scalar(self, val):
 		
 		result = FuzzySet(f'({self._name}) min ({val})', self._domain_min, self._domain_max, self._res)
-		result._dom = np.minimum(self._dom, val)
+		result._membership_degree_list = np.minimum(self._membership_degree_list, val)
 
 		return result
 
 	def union(self, f_set):
 
 		result = FuzzySet(f'({self._name}) union ({f_set._name})', self._domain_min, self._domain_max, self._res)
-		result._dom = np.maximum(self._dom, f_set._dom)
+		result._membership_degree_list = np.maximum(self._membership_degree_list, f_set._membership_degree_list)
 
 		return result
 
 	def intersection(self, f_set):
 
 		result = FuzzySet(f'({self._name}) intersection ({f_set._name})', self._domain_min, self._domain_max, self._res)
-		result._dom = np.minimum(self._dom, f_set._dom)
+		result._membership_degree_list = np.minimum(self._membership_degree_list, f_set._membership_degree_list)
 
 		return result
 
 	def complement(self):
 
 		result = FuzzySet(f'not ({self._name})', self._domain_min, self._domain_max, self._res)
-		result._dom = 1 - self._dom
+		result._membership_degree_list = 1 - self._membership_degree_list
 
 		return result
 
 	def cog_defuzzify(self):
 		
-		num = np.sum(np.multiply(self._dom, self._domain))
-		den = np.sum(self._dom)
-
+		num = np.sum(np.multiply(self._membership_degree_list, self._domain))
+		den = np.sum(self._membership_degree_list)
+		assert den != 0
 		return num/den
 
 	def domain_elements(self):
 		return self._domain
 
 	def dom_elements(self):
-		return self._dom
+		return self._membership_degree_list
 
 	def plot_set(self, ax, col=''):
-		ax.plot(self._domain, self._dom, col)
+		ax.plot(self._domain, self._membership_degree_list, col)
 		ax.set_ylim([-0.1,1.1])
 		ax.set_title(self._name)
 		ax.grid(True, which='both', alpha=0.4)
